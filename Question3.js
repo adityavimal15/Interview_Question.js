5d28a8e98d59164344898bf48d342e9750eb6ec7
Aditya Vimal <139321429+adityavimal15@users.noreply.github.com>
2025-02-24 15:53:23 +0530

    Add files via upload

---
+++ Question3.js
@@ -0,0 +1,12 @@
+const [a,b]=[10,"Hi"]
+console.log(a,b);
+
+// By using d destructor
+
+const [...c]=[10,20,30,40,50];
+console.log(c[4]);
+
+// By using Object destructor
+
+const {3:d}=[10,20,30,40,50,60,70,80,90]
+console.log(d);
