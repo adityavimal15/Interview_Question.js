b91cc8dc5286ef93ae375cad94030e67794d4392
Aditya Vimal <139321429+adityavimal15@users.noreply.github.com>
2025-02-24 15:53:41 +0530

    Add files via upload

---
+++ Question4.js
@@ -0,0 +1,12 @@
+function abc(){
+    console.log("Hii");
+}
+
+let value= new abc();
+
+console.log(value);
+
+// abc() is print a Hii and new will create a new empty object
+// Therefore, output is 
+// Hii
+// abc {}
